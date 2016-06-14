c129b5c9c079c5e3164a12d7129ac8d265a145c3
Ionică Bizău <bizauionica@gmail.com>
2016-06-14 09:16:03 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -2,4 +2,14 @@
 
 const objKeysCount = require("../lib");
 
-console.log(objKeysCount());
+console.log(objKeysCount({}, 0));
+// => true
+
+console.log(objKeysCount({ foo: 42}, 1, true));
+// => "foo"
+
+console.log(objKeysCount({}, 1));
+// => false
+
+console.log(objKeysCount({ foo: 42, bar: "baz" }, 2));
+// => true
